bba85c061419cfaf32ffaa397e60299a8116e055
henrikszucs <47735877+henrikszucs@users.noreply.github.com>
2023-10-17 13:04:08 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 "use strict";
 
+//HTML connenct
+window.addEventListener("load", function() {
+    document.getElementById("start").addEventListener("click", function() {
+        main();
+    });
+});
 
+
+// Helper functions
+let getFile = async function(src) {
+    return new Promise((resolve) => {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onload = function() {
+            if (xhr.readyState === 4) {
+                resolve(this.response);
+            }
+        }
+        xhttp.open("GET", src, true);
+        xhttp.send();
+    });
+}
+
+//Webassembly use
+let main = async function() {
+
+    
+};
 
 
 
