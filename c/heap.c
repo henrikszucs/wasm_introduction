40403eed827662f93ef208fe7f1a46ee4b0381cf
henrikszucs <47735877+henrikszucs@users.noreply.github.com>
2023-10-29 16:39:41 +0100

    update

--- c/heap.c
+++ c/heap.c
@@ -4,13 +4,19 @@
 int main() { 
   return 42;
 }
+int * ptr;
 
-int * test() {
+int * test(int offset) {
   // Dynamically allocate memory using malloc()
   int n = 40;
-  int * ptr = (int*)malloc(n * sizeof(int));
+  ptr = (int*)malloc(n * sizeof(int));
   for (int i = 0; i < n; ++i) {
-    ptr[i] = i + 1;
+    ptr[i] = i + offset;
   }
+
   return &ptr[20];
+}
+
+void js_free() {
+  free(ptr);
 }
--- c/heap.js
+++ c/heap.js
@@ -1,8 +1,20 @@
 var wasmModule = new WebAssembly.Module(wasmCode);
 var wasmInstance = new WebAssembly.Instance(wasmModule, wasmImports);
 
-let index = wasmInstance.exports.test();
-index = Math.floor(index / Int32Array.BYTES_PER_ELEMENT);
-let arr = new Int32Array(wasmInstance.exports.memory.buffer);
+let index = wasmInstance.exports.test(10);
+let memoryBuffer = wasmInstance.exports.memory.buffer;
+let memoryArray = new Int8Array(memoryBuffer);
+selected = new DataView(memoryBuffer, index, Int32Array.BYTES_PER_ELEMENT);
+
 console.log(index);
-console.log(arr);
+console.log(selected.getInt32(0, true));
+
+wasmInstance.exports.js_free();
+
+index = wasmInstance.exports.test(100);
+
+console.log(memoryArray);
+
+
+
+
